e149f010a306e05c45034b93b4fb08a70a54c0a1
KristhianCacho <kristhiancacho518@gmail.com>
2024-02-22 02:53:07 +0000

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,68 +1,41 @@
 #!/usr/bin/python3
-"""
-Usings API for extract information false.
-"""
-
-from sys import argv
+"""This module gathers data from an API"""
 import json
 import requests
 
-
-def main():
-    """
-    Query name and tasks of employee.
-    """
-
-    all_employees = {}
-
-    for id in range(1, 11):
-        """
-        Use API URL to get user tasks and information
-        """
-
-        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
-        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-
-        response = requests.get(url_id)
-
-        if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
-            exit()
-
-        data = response.json()
-        EMPLOYEE_NAME = data["username"]
-
-        response = requests.get(url_todos)
-
-        if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
-            exit()
-
-        todos = response.json()
-        list_todos = []
-        dict_todos = {}
-
-        all_tasks = [todo["title"] for todo in todos]
-        status_task = [todo["completed"] for todo in todos]
-
-        for index in range(0, len(all_tasks)):
-            dict_todos = {
-                "username": EMPLOYEE_NAME,
-                "task": all_tasks[index],
-                "completed": status_task[index]
-            }
-
-            list_todos.append(dict_todos)
-
-            employee_todos = {str(id): list_todos}
-
-        all_employees.update(employee_todos)
-
-        name_file_json = "todo_all_employees.json"
-
-        with open(name_file_json, mode="w", newline='') as f:
-            json.dump(all_employees, f, indent=4)
-
-
 if __name__ == "__main__":
-    main()
+
+    # Make requests in parallel to reduce overhead
+    session = requests.Session()
+    response_todos = session.get("https://jsonplaceholder.typicode.com/todos")
+    response_users = session.get("https://jsonplaceholder.typicode.com/users")
+
+    # Check if the requests were successful (status code 200)
+    if response_todos.status_code != 200 or response_users.status_code != 200:
+        print("Failed to fetch data.")
+        print(f"TODOS response status code: {response_todos.status_code}")
+        print(f"Users response status code: {response_users.status_code}")
+        exit(1)
+
+    todos = response_todos.json()
+    users = response_users.json()
+
+    todo_all_employees = {}
+    for user in users:
+        user_id = user.get("id")
+        todo_all_employees[user_id] = []
+
+    for todo in todos:
+        user_id = todo.get("userId")
+        if user_id in todo_all_employees:
+            user = [user for user in users if user["id"] == user_id][0]
+            todo_all_employees[user_id].append({
+                "username": user.get("username"),
+                "task": todo["title"],
+                "completed": todo["completed"]
+            })
+
+    # Export to JSON region
+    filename = "todo_all_employees.json"
+    with open(filename, mode="w") as file:
+        json.dump(todo_all_employees, file, indent=4)
