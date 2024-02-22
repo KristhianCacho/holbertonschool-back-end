347eb5f109b84ffef8a3d78f4c361fca39a8d9e6
KristhianCacho <kristhiancacho518@gmail.com>
2024-02-22 02:54:52 +0000

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,61 +1,51 @@
 #!/usr/bin/python3
 """
-Usings API
+Script that use
+https://jsonplaceholder.typicode.com/guide/
+to get information and export it as csv
 """
-
 import json
 import requests
-from sys import argv
+from sys import argv, stderr, exit
 
 
 def main():
-    """
-    Query name and tasks of employee
-    """
-    if len(argv) >= 2 and argv[1].isdigit():
-        id = argv[1]
+    if len(argv) < 2:
+        print("Usage: {} ID".format(argv[0]), file=stderr)
+        exit(1)
 
-        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
-        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+    employee_id = int(argv[1])
+    url_to = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response_todo = requests.get(url_to)
+    url_username = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response_username = requests.get(url_username)
 
-        response = requests.get(url_id)
-
-        if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
-            exit()
-
-        data = response.json()
-        EMPLOYEE_NAME = data["username"]
-
-        response = requests.get(url_todos)
-
-        if response.status_code != 200:
-            print(f"Error:username not found. Please enter an existing user id")
-            exit()
-
-        todos = response.json()
-        list_todos = []
-        dict_todos = {}
-
-        all_tasks = [todo["title"] for todo in todos]
-        status_task = [todo["completed"] for todo in todos]
-
-        for index in range(0, len(all_tasks)):
-            dict_todos = {
-                "task": all_tasks[index],
-                "completed": status_task[index],
-                "username": EMPLOYEE_NAME}
-
-            list_todos.append(dict_todos)
-
-            employee_todos = {str(id): list_todos}
-
-        name_file_json = f"{id}.json"
-
-        with open(name_file_json, mode="w", newline="") as f:
-            json.dump(employee_todos, f, indent=4)
+    if response_todo.status_code == 200:
+        todos = response_todo.json()
     else:
-        print("Please enter an existing user id")
+        print("Error fetching TODO list")
+
+    if response_username.status_code == 200:
+        employee_data = response_username.json()
+        if "username" in employee_data:
+            employee_username = employee_data.get("username")
+    else:
+        print("Error fetching employee username")
+
+    tasks_list = [
+            {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": employee_username
+            }
+            for todo in todos
+        ]
+
+    json_data = json.dumps({employee_id: tasks_list})
+    with open(f"{employee_id}.json", "w") as f:
+        f.write(json_data)
+
+    print("Data exported to {}".format(f.name))
 
 
 if __name__ == "__main__":
